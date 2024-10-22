b656dca7c8415b7268ac7b4f7e7cb3094823502a
Arela-Cristina <instagrandine@gmail.com>
2024-10-22 15:55:22 +0200

    Add event listener. Properties and methods for img and botton

--- main.js
+++ main.js
@@ -19,5 +19,18 @@ console.log(offBulb)
 //test click onButton
 onButton.addEventListener('click', function () {
     console.log('Hai cliccato');
-
+    //propieta .src per consultare il atributo src della immagine
+    //metodo includes per consultare se essiste una determinata 'stringa' nel atributo src.
+    //essiste (includes.) 'img/white_lamp.png' nel elemento offBulb?
+    if (offBulb.src.includes('img/white_lamp.png')){
+        //se essiste ed e spenta accenderla, al click/ Proprieta src. ancora per manipoalre il atributo
+        offBulb.src = 'img/yellow_lamp.png';
+        //proprieta textContent. per modificare il testo del bottone
+        onButton.textContent = 'Spegni'
+    } else {
+        //se e accesa la spegniamo
+        offBulb.src = 'img/white_lamp.png';
+        //proprieta textContent. per modificare il testo del bottone
+        onButton.textContent = 'Accendi'
+    }
 });
