67b8560c5f5e3882b4cb762540c819d2b7fc7906
Arela-Cristina <instagrandine@gmail.com>
2024-10-22 16:30:06 +0200

    refactor naming

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 //prima chiamiamo il elemento del DOM che contiene il botone
-const onButton = document.getElementById('onButton');
+const button = document.getElementById('button');
 
 //chiamiamo il elemento del DOM che contiene la lampadina
-const offBulb = document.getElementById('offBulb');
-console.log(offBulb)
+const bulb = document.getElementById('bulb');
+console.log(bulb)
 
 
-//test click onButton
-onButton.addEventListener('click', function () {
+//test click button
+button.addEventListener('click', function () {
     console.log('Hai cliccato');
     //propieta .src per consultare il atributo src della immagine
     //metodo includes per consultare se essiste una determinata 'stringa' nel atributo src.
-    //essiste (includes.) 'img/white_lamp.png' nel elemento offBulb?
-    if (offBulb.src.includes('img/white_lamp.png')){
+    //essiste (includes.) 'img/white_lamp.png' nel elemento offbulb?
+    if (bulb.src.includes('img/white_lamp.png')){
         //se essiste ed e spenta accenderla, al click/ Proprieta src. ancora per manipoalre il atributo
-        offBulb.src = 'img/yellow_lamp.png';
+        bulb.src = 'img/yellow_lamp.png';
         //proprieta textContent. per modificare il testo del bottone
-        onButton.textContent = 'Spegni'
+        button.textContent = 'Spegni'
     } else {
         //se e accesa la spegniamo
-        offBulb.src = 'img/white_lamp.png';
+        bulb.src = 'img/white_lamp.png';
         //proprieta textContent. per modificare il testo del bottone
-        onButton.textContent = 'Accendi'
+        button.textContent = 'Accendi'
     }
 });
